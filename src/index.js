4625e57dbaade5a25c58ea822269c81776ee6b85
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2025-11-13 09:00:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import app from "./app.js"
-import dotenv from "dotenv"
-import connect from './db/dbConnection.js'
+import app from "./app.js";
+import dotenv from "dotenv";
+import connect from "./db/dbConnection.js";
 
 dotenv.config({
-path:'./.env'
-})
+  path: "./.env",
+});
 
-connect().then(()=>{
-app.listen(process.env.PORT,()=>{
-console.log(`Server is running at port ${process.env.PORT}`);
-})
-})
-.catch((err)=>{
-console.log("some errors to running",err);
-})
+connect()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`✅ Server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("❌ Some error while running the server:", err);
+  });
 
 /*
 let app=express()
