6cae4d9add085cf9e0544e079afb392c4ddaaf95
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2025-11-13 08:09:29 +0530

    Update dbConnection.js

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,16 +1,8 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
-
 const connect = async () => {
-if (isConnected) {
-console.log("✅ MongoDB already connected");
-return;
-}
-
 try {
 const conn = await mongoose.connect(process.env.MONGODB_URL);
-isConnected = true;
 console.log(✅ MongoDB Connected: ${conn.connection.host});
 } catch (error) {
 console.error("❌ MongoDB connection failed:", error.message);
