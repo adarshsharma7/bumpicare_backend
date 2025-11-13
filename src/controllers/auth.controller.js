5080c2fc4e1dc25810c0a7fe9b8a5a3d82ea4563
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2025-11-13 09:12:47 +0530

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 import { generateToken } from "../utils/generateToken.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
 
 // ✅ Register Controller
 export const registerUser = async (req, res) => {
